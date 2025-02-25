c0be5de3506d1789bff0e66bd3586e7410c291fc
user <sairysss@gmail.com>
2025-02-25 18:32:35 +0100

    updated configs

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -425,6 +425,7 @@ return {
     -- "nvim-telescope/telescope-fzf-native.nvim",
     -- build = "make",
     -- },
+    enabled = false,
     config = function()
       local dropbar_api = require("dropbar.api")
       vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
@@ -476,4 +477,23 @@ return {
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
+
+  -- Python --
+  -- {
+  --   "linux-cultist/venv-selector.nvim",
+  --   dependencies = {
+  --     "neovim/nvim-lspconfig",
+  --     "mfussenegger/nvim-dap",
+  --     "mfussenegger/nvim-dap-python", --optional
+  --     { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  --   },
+  --   lazy = false,
+  --   branch = "regexp", -- This is the regexp branch, use this for the new version
+  --   config = function()
+  --     require("venv-selector").setup()
+  --   end,
+  --   keys = {
+  --     { ",v", "<cmd>VenvSelect<cr>" },
+  --   },
+  -- },
 }
