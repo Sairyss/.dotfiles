6d852c33879c6987ee294f8acdd494db13f9ff99
user <sairysss@gmail.com>
2025-03-18 14:56:04 +0100

    nvim: updated shortcuts for snacks picker

--- .config/nvim/lua/plugins/plugin_options.lua
+++ .config/nvim/lua/plugins/plugin_options.lua
@@ -158,6 +158,26 @@ return {
         end,
         desc = "Resume",
       },
+      { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+      { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+      { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+      { "<leader>sG", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
+      { "<leader>sg", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+      { "<leader>sW", LazyVim.pick("grep_word"), desc = "Visual selection or word (Root Dir)", mode = { "n", "x" } },
+      {
+        "<leader>sw",
+        LazyVim.pick("grep_word", { root = false }),
+        desc = "Visual selection or word (cwd)",
+        mode = { "n", "x" },
+      },
+      { "<leader>fR", LazyVim.pick("oldfiles"), desc = "Recent" },
+      {
+        "<leader>fr",
+        function()
+          Snacks.picker.recent({ filter = { cwd = true } })
+        end,
+        desc = "Recent (cwd)",
+      },
     },
   },
   {
