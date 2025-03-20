fc1dcc0bf32941ba941d3e509e61ecb0fd5dc076
user <sairysss@gmail.com>
2025-03-20 15:33:10 +0100

    updated configs

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -487,23 +487,19 @@ return {
       })
     end,
   },
-
-  -- Python --
-  -- {
-  --   "linux-cultist/venv-selector.nvim",
-  --   dependencies = {
-  --     "neovim/nvim-lspconfig",
-  --     "mfussenegger/nvim-dap",
-  --     "mfussenegger/nvim-dap-python", --optional
-  --     { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-  --   },
-  --   lazy = false,
-  --   branch = "regexp", -- This is the regexp branch, use this for the new version
-  --   config = function()
-  --     require("venv-selector").setup()
-  --   end,
-  --   keys = {
-  --     { ",v", "<cmd>VenvSelect<cr>" },
-  --   },
-  -- },
+  {
+    "Pocco81/auto-save.nvim",
+    config = function()
+      require("auto-save").setup({
+        -- your config goes here
+      })
+    end,
+  },
+  {
+    "arnamak/stay-centered.nvim",
+    lazy = false,
+    opts = {
+      -- skip_filetypes = { "lua", "typescript" },
+    },
+  },
 }
