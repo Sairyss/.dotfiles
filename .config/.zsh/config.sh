6ff132ef6ce11126b94a6287dc0aedd7f139675c
user <sairysss@gmail.com>
2025-08-09 22:16:06 +0200

    fix zsh substring search

--- .config/.zsh/config.sh
+++ .config/.zsh/config.sh
@@ -21,5 +21,11 @@ zvm_after_init_commands+=(eval "$(atuin init zsh --disable-up-arrow)") # https:/
 source $CONFIG_DIR/plugins/aichat/aichat_autocomplete.sh
 zvm_after_init_commands+=("source $CONFIG_DIR/plugins/aichat/aichat_integration.zsh")
 
+# Bind up and down and j k to substring search history
+zvm_after_init_commands+=("bindkey \"${terminfo[kcuu1]}\" history-substring-search-up")
+zvm_after_init_commands+=("bindkey \"${terminfo[kcud1]}\" history-substring-search-down")
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
