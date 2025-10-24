048b8fb53c96d2fe5b63d847f916dacc7f5213c5
user <sairysss@gmail.com>
2025-10-24 15:44:25 +0200

    updated neovim AI plugins

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -5,8 +5,18 @@ return {
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
-      provider = "copilot",
-      auto_suggestions_provider = "copilot",
+      provider = "gemini",
+      auto_suggestions_provider = "gemini",
+      behaviour = {
+        auto_suggestions = false,
+      },
+      input = {
+        provider = "snacks",
+        provider_opts = {
+          title = "Avante Input",
+          icon = " ",
+        },
+      },
       providers = {
         copilot = {
           endpoint = "https://api.githubcopilot.com",
@@ -20,7 +30,7 @@ return {
           },
         },
         gemini = {
-          model = "gemini-2.5-pro",
+          model = "gemini-2.5-flash",
           -- temperature = 0,
           temperature = 0,
           max_tokens = 8192,
@@ -58,7 +68,7 @@ return {
       --- The below dependencies are optional,
       -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      -- "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
@@ -87,6 +97,78 @@ return {
     },
   },
   {
+    "milanglacier/minuet-ai.nvim",
+    config = function()
+      require("minuet").setup({
+        provider = "gemini",
+        blink = {
+          enable_auto_complete = true,
+        },
+        provider_options = {
+          gemini = {
+            model = "gemini-2.5-flash",
+            -- system = "see [Prompt] section for the default value",
+            -- few_shots = "see [Prompt] section for the default value",
+            -- chat_input = "See [Prompt Section for default value]",
+            stream = true,
+            api_key = "GEMINI_API_KEY",
+            end_point = "https://generativelanguage.googleapis.com/v1beta/models",
+            optional = {
+              generationConfig = {
+                maxOutputTokens = 512,
+                -- When using `gemini-2.5-flash`, it is recommended to entirely
+                -- disable thinking for faster completion retrieval.
+                thinkingConfig = {
+                  thinkingBudget = 0,
+                },
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "Kaiser-Yang/blink-cmp-avante",
+      -- ... Other dependencies
+    },
+    config = function(_, opts)
+      opts.keymap = vim.tbl_deep_extend("force", opts.keymap or {}, {
+        ["<A-y>"] = require("minuet").make_blink_map(),
+      })
+      require("blink-cmp").setup(opts)
+    end,
+    opts = {
+      sources = {
+        default = { "avante", "minuet", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          avante = {
+            module = "blink-cmp-avante",
+            name = "Avante",
+            opts = {
+              -- options for blink-cmp-avante
+            },
+          },
+          minuet = {
+            name = "minuet",
+            module = "minuet.blink",
+            async = true,
+            -- Should match minuet.config.request_timeout * 1000,
+            -- since minuet.config.request_timeout is in seconds
+            timeout_ms = 3000,
+            score_offset = 50, -- Gives minuet higher priority among suggestions
+          },
+        },
+      },
+    },
+    -- keymap = {
+    --   -- Manually invoke minuet completion.
+    --   -- ["<A-y>"] = require("minuet").make_blink_map(),
+    -- },
+  },
+  {
     "NickvanDyke/opencode.nvim",
     dependencies = {
       "folke/snacks.nvim",
@@ -96,7 +178,7 @@ return {
     ---  -- Your configuration, if any
     ---},
   -- stylua: ignore
-  keys = {
+    keys = {
     -- opencode.nvim exposes a general, flexible API — customize it to your workflow!
     -- But here are some examples to get you started :)
       { '<leader>At', function() require('opencode').toggle() end, desc = 'Toggle opencode', },
@@ -111,77 +193,77 @@ return {
       { '<leader>At', function() require('opencode').prompt('Add tests for @selection') end, desc = 'Test selection', mode = 'v', },
     },
   },
-  {
-    "folke/sidekick.nvim",
-    opts = {
-      -- add any options here
-      cli = {
-        mux = {
-          backend = "zellij",
-          enabled = true,
-        },
-      },
-    },
-    keys = {
-      {
-        "<tab>",
-        function()
-          -- if there is a next edit, jump to it, otherwise apply it if any
-          if not require("sidekick").nes_jump_or_apply() then
-            return "<Tab>" -- fallback to normal tab
-          end
-        end,
-        expr = true,
-        desc = "Goto/Apply Next Edit Suggestion",
-      },
-      {
-        "<c-.>",
-        function()
-          require("sidekick.cli").focus()
-        end,
-        mode = { "n", "x", "i", "t" },
-        desc = "Sidekick Switch Focus",
-      },
-      {
-        -- doesn't work when binding from avante config, so doing it here
-        "<leader>aa",
-        function()
-          require("avante").toggle()
-        end,
-        desc = "avante: toggle",
-      },
-      {
-        "<leader>aA",
-        function()
-          require("sidekick.cli").toggle({ focus = true })
-        end,
-        desc = "Sidekick Toggle CLI",
-        mode = { "n", "v" },
-      },
-      -- {
-      --   "<leader>ac",
-      --   function()
-      --     require("sidekick.cli").toggle({ name = "claude", focus = true })
-      --   end,
-      --   desc = "Sidekick Claude Toggle",
-      --   mode = { "n", "v" },
-      -- },
-      -- {
-      --   "<leader>ag",
-      --   function()
-      --     require("sidekick.cli").toggle({ name = "grok", focus = true })
-      --   end,
-      --   desc = "Sidekick Grok Toggle",
-      --   mode = { "n", "v" },
-      -- },
-      {
-        "<leader>ap",
-        function()
-          require("sidekick.cli").prompt()
-        end,
-        desc = "Sidekick Ask Prompt",
-        mode = { "n", "v" },
-      },
-    },
-  },
+  -- {
+  --   "folke/sidekick.nvim",
+  --   opts = {
+  --     -- add any options here
+  --     cli = {
+  --       mux = {
+  --         backend = "zellij",
+  --         enabled = true,
+  --       },
+  --     },
+  --   },
+  --   keys = {
+  --     {
+  --       "<tab>",
+  --       function()
+  --         -- if there is a next edit, jump to it, otherwise apply it if any
+  --         if not require("sidekick").nes_jump_or_apply() then
+  --           return "<Tab>" -- fallback to normal tab
+  --         end
+  --       end,
+  --       expr = true,
+  --       desc = "Goto/Apply Next Edit Suggestion",
+  --     },
+  --     {
+  --       "<c-.>",
+  --       function()
+  --         require("sidekick.cli").focus()
+  --       end,
+  --       mode = { "n", "x", "i", "t" },
+  --       desc = "Sidekick Switch Focus",
+  --     },
+  --     {
+  --       -- doesn't work when binding from avante config, so doing it here
+  --       "<leader>aa",
+  --       function()
+  --         require("avante").toggle()
+  --       end,
+  --       desc = "avante: toggle",
+  --     },
+  --     {
+  --       "<leader>aA",
+  --       function()
+  --         require("sidekick.cli").toggle({ focus = true })
+  --       end,
+  --       desc = "Sidekick Toggle CLI",
+  --       mode = { "n", "v" },
+  --     },
+  --     -- {
+  --     --   "<leader>ac",
+  --     --   function()
+  --     --     require("sidekick.cli").toggle({ name = "claude", focus = true })
+  --     --   end,
+  --     --   desc = "Sidekick Claude Toggle",
+  --     --   mode = { "n", "v" },
+  --     -- },
+  --     -- {
+  --     --   "<leader>ag",
+  --     --   function()
+  --     --     require("sidekick.cli").toggle({ name = "grok", focus = true })
+  --     --   end,
+  --     --   desc = "Sidekick Grok Toggle",
+  --     --   mode = { "n", "v" },
+  --     -- },
+  --     {
+  --       "<leader>ap",
+  --       function()
+  --         require("sidekick.cli").prompt()
+  --       end,
+  --       desc = "Sidekick Ask Prompt",
+  --       mode = { "n", "v" },
+  --     },
+  --   },
+  -- },
 }
