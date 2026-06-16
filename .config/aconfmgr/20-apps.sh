fb14071538c447cc1eb2295670c8511cea07762a
user <sairysss@gmail.com>
2026-06-16 11:48:07 +0200

    aconfmgr: updated apps list

--- .config/aconfmgr/20-apps.sh
+++ .config/aconfmgr/20-apps.sh
@@ -1,16 +1,13 @@
-AddPackage alacritty               # A cross-platform, GPU-accelerated terminal emulator
 AddPackage atuin                   # Magical shell history
 AddPackage bat                     # Cat clone with syntax highlighting and git integration
 AddPackage broot                   # Fuzzy Search + tree + cd
 AddPackage coolreader              # Fast and small XML/CSS based eBook reader
 AddPackage cryfs                   # Cryptographic filesystem for the cloud
 AddPackage cmake                   # A cross-platform open-source make system
-AddPackage ctop                    # Top-like interface for container metrics
 AddPackage dbeaver                 # Free universal SQL Client for developers and database administrators (community edition)
 AddPackage docker                  # Pack, ship and run any application as a lightweight container
 AddPackage docker-compose          # Fast, isolated development environments using Docker
 AddPackage emacs                   # The extensible, customizable, self-documenting real-time display editor
-AddPackage erdtree                 # A multi-threaded filesystem and disk-usage analysis tool
 AddPackage eza                     # A modern replacement for ls (community fork of exa)
 AddPackage zoxide                  # Command-line productivity booster, offers quick access to files and directories
 AddPackage fd                      # Simple, fast and user-friendly alternative to find
@@ -35,11 +32,8 @@ AddPackage neovim                  # Fork of Vim aiming to improve user experien
 AddPackage nodejs                  # Evented I/O for V8 javascript
 AddPackage npm                     # A package manager for JavaScript
 AddPackage opensnitch              # A GNU/Linux application firewall
-AddPackage peek                    # Simple screen recorder with an easy to use interface
-AddPackage plantuml                # Component that allows to quickly write uml diagrams
 AddPackage pgcli                   # A command line interface for PostgreSQL with auto-completion and syntax highlighting
 AddPackage python                  # Next generation of the python high-level scripting language
-AddPackage ranger                  # Simple, vim-like file manager
 AddPackage ripgrep                 # A search tool that combines the usability of ag with the raw speed of grep
 AddPackage rofi                    # A window switcher, application launcher and dmenu replacement
 AddPackage sed                     # GNU stream editor
@@ -62,33 +56,26 @@ AddPackage unzip                   # For extracting and viewing files in .zip ar
 AddPackage visidata                # Terminal spreadsheet multitool for discovering and arranging data
 AddPackage wine                    # A compatibility layer for running Windows programs
 AddPackage winetricks              # Script to install various redistributable runtime libraries in Wine.
-AddPackage xdotool                 # Command-line X11 automation tool
 AddPackage yank                    # Copy terminal output to clipboard
 AddPackage yarn                    # Fast, reliable, and secure dependency management
 AddPackage yay                     # Yet another yogurt. Pacman wrapper and AUR helper written in go. Pre-compiled.
 AddPackage yazi                    # Blazing fast terminal file manager written in Rust, based on async I/O
 AddPackage zellij                  # A terminal multiplexer
-AddPackage zk                      # A command-line tool helping you to maintain a Zettelkasten or personal wiki
 AddPackage zsh                     # A very advanced and programmable command interpreter (shell) for UNIX
 AddPackage zsh-completions         # Additional completion definitions for Zsh
 
 # Foreign packages
-AddPackage --foreign aconfmgr-git                 # A configuration manager for Arch Linux
-AddPackage --foreign birdtray                     # Run Thunderbird with a system tray icon.
-AddPackage --foreign google-chrome                # The popular and trusted web browser by Google (Stable Channel)
-AddPackage --foreign headset-charge-indicator-git # Simple app-indicator for GNOME desktops to display the battery charge of some wireless headsets
-AddPackage --foreign headsetcontrol               # Sidetone and Battery status for Logitech G930, G533, G633, G933 SteelSeries Arctis 7/PRO 2019 and Corsair VOID (Pro) in Linux and MacOSX
-AddPackage --foreign kanata                       # Bring the customizability of a QMK board to any keyboard near you
-AddPackage --foreign lazydocker                   # A simple terminal UI for docker and docker-compose, written in Go with the gocui library.
-AddPackage --foreign mprocs                       # Run multiple commands in parallel
-AddPackage --foreign opensnitch-ebpf-module       # eBPF process monitor module for opensnitch
-AddPackage --foreign plex-media-server            # The back-end media server component of Plex.
-AddPackage --foreign postman-bin                  # Build, test, and document your APIs faster
-AddPackage --foreign scm_breeze-git               # SCM Breeze is a set of shell scripts (for bash and zsh) that enhance your interaction with git.
-AddPackage --foreign slack-desktop                # Slack Desktop (Beta) for Linux
-AddPackage --foreign sqls                         # Language Server Protocol implementation for SQL
-AddPackage --foreign visual-studio-code-bin       # Visual Studio Code (vscode): Editor for building and debugging modern web and cloud applications (official binary version)
-AddPackage --foreign zeal-git                     # A simple documentation browser
-AddPackage --foreign zoom                         # Video Conferencing and Web Conferencing Service
-AddPackage --foreign zotero-bin                   # Zotero Standalone. Is a free, easy-to-use tool to help you collect, organize, cite, and share your research sources.
-AddPackage --foreign zsh-syntax-highlighting-git  # Fish shell like syntax highlighting for Zsh (from git)
+AddPackage --foreign aconfmgr-git           # A configuration manager for Arch Linux
+AddPackage --foreign google-chrome          # The popular and trusted web browser by Google (Stable Channel)
+AddPackage --foreign headsetcontrol         # Sidetone and Battery status for Logitech G930, G533, G633, G933 SteelSeries Arctis 7/PRO 2019 and Corsair VOID (Pro) in Linux and MacOSX
+AddPackage --foreign kanata                 # Bring the customizability of a QMK board to any keyboard near you
+AddPackage --foreign lazydocker             # A simple terminal UI for docker and docker-compose, written in Go with the gocui library.
+AddPackage --foreign mprocs                 # Run multiple commands in parallel
+AddPackage --foreign opensnitch-ebpf-module # eBPF process monitor module for opensnitch
+AddPackage --foreign postman-bin            # Build, test, and document your APIs faster
+AddPackage --foreign slack-desktop          # Slack Desktop (Beta) for Linux
+AddPackage --foreign sqls                   # Language Server Protocol implementation for SQL
+AddPackage --foreign visual-studio-code-bin # Visual Studio Code (vscode): Editor for building and debugging modern web and cloud applications (official binary version)
+AddPackage --foreign zeal-git               # A simple documentation browser
+AddPackage --foreign zoom                   # Video Conferencing and Web Conferencing Service
+AddPackage --foreign zotero-bin             # Zotero Standalone. Is a free, easy-to-use tool to help you collect, organize, cite, and share your research sources.
--- .config/aconfmgr/29-old_apps.sh
+++ .config/aconfmgr/29-old_apps.sh
@@ -5,3 +5,17 @@
 # AddPackage helix                # A post-modern modal text editor
 # AddPackage neovide              # No Nonsense Neovim Client in Rust
 # AddPackage navi                 # An interactive cheatsheet tool for the command-line
+# AddPackage alacritty               # A cross-platform, GPU-accelerated terminal emulator
+# AddPackage ctop                    # Top-like interface for container metrics
+# AddPackage erdtree                 # A multi-threaded filesystem and disk-usage analysis tool
+# AddPackage jellyfin-server         # Jellyfin server backend
+# AddPackage peek                    # Simple screen recorder with an easy to use interface
+# AddPackage plantuml                # Component that allows to quickly write uml diagrams
+# AddPackage ranger                  # Simple, vim-like file manager
+# AddPackage xdotool                 # Command-line X11 automation tool
+# AddPackage zk                      # A command-line tool helping you to maintain a Zettelkasten or personal wiki
+# AddPackage --foreign birdtray                     # Run Thunderbird with a system tray icon.
+# AddPackage --foreign headset-charge-indicator-git # Simple app-indicator for GNOME desktops to display the battery charge of some wireless headsets
+# AddPackage --foreign plex-media-server           # The back-end media server component of Plex.
+# AddPackage --foreign scm_breeze-git              # SCM Breeze is a set of shell scripts (for bash and zsh) that enhance your interaction with git.
+# AddPackage --foreign zsh-syntax-highlighting-git # Fish shell like syntax highlighting for Zsh (from git)
